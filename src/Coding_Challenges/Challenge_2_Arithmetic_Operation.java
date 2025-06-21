0d93eca62820cb773a8f43b1b729f5da557f59e8
Abirami-R03 <abiravichandran5@gmail.com>
2025-06-21 12:30:10 +0530

    Challenge 2 done

---
+++ src/Coding_Challenges/Challenge_2_Arithmetic_Operation.java
@@ -0,0 +1,18 @@
+package Coding_Challenges;
+
+public class Challenge_2_Arithmetic_Operation {
+    public static void main(String[] args) {
+        int a = 10, b= 5;
+        int add = a+b;
+        System.out.println("Addition:" + add);
+        int minus =  a-b;
+        System.out.println("Subtraction:" + minus);
+        int multi = a*b;
+        System.out.println("Multiplication:" + multi);
+        int division = a/b;
+        System.out.println("Division:" + division);
+        int mod = a%b;
+        System.out.println("Modulus:" + mod);
+
+    }
+}
